bdca211dad8a0107d91ad16d579d62997f2bd9d0
Bradley <47512745+bgrose@users.noreply.github.com>
2020-06-13 20:49:25 -0400

    creates templates for coding

---
+++ Introduction/template.py
@@ -0,0 +1,51 @@
+# Example 1
+def exercise1():
+    print("TODO 1")
+
+# Example 2
+def exercise2(x,y):
+    print("TODO 2")
+
+# Example 3
+def exercise3(n):
+    print("TODO 3")
+
+# Example 4
+def exercise4(miles):
+    print("TODO 4")
+
+# Example 5
+def exercise5(age):
+    print("TODO 5")
+
+# Example 6
+list = ['Keanu', 'River', 'Leonardo', 'Brad', 'A', '1998']
+
+def exercise6():
+    print("TODO 6")
+
+# Example 7
+def exercise7():
+   print("TODO 7")
+
+# Example 8
+def exercise8(mon, day, year):
+    months = {'January':1, 'February':2, 'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 'August':8, 'September':9, 'October':10, 'November':11, 'December':12}
+    print("TODO")
+
+# Example 9
+def exercise9(ele):
+    import csv
+    f = open("Introduction\periodictable.csv")
+    print("TODO")
+
+#Tester
+exercise1()
+exercise2(2, 5)
+exercise3(22)
+exercise4(5)
+exercise5(22)
+exercise6()
+exercise7()
+exercise8("June", 24, 1998)
+exercise9("Gold")
---
+++ Projects/template.py
@@ -0,0 +1,22 @@
+
+# Over Under Game
+def overUnder():
+    import random
+    print("TODO overUnder")
+
+#Math Problem Simulator
+def mathGame():
+    import random
+    print("TODO mathGame")
+
+
+def changeMaker():
+    print("TODO changeMaker")
+    
+
+
+#Tester
+
+overUnder()
+mathGame()
+changeMaker()
