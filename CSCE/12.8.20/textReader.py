206cefe9056fc2e6efb35749aee3fefa6ec747fd
bgrose <47512745+bgrose@users.noreply.github.com>
2020-12-08 13:19:07 -0500

    update

--- CSCE/12.8.20/textReader.py
+++ CSCE/12.8.20/textReader.py
@@ -4,7 +4,7 @@ if os.path.isfile("file.txt"):
     file = open("file.txt", 'r+')
     line = file.read()
     print(line)
-    file.write("\nHello, Midterm")
+    file.write("\nHello, Midterm!")
     line = file.read()
     print(line)
     file.close()
